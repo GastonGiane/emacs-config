e6560841737b67d1bae51aa4936d6913058e011e
Gaston Giane <gaston@debian>
2021-04-06 11:55:42 -0300

    Add evil mode

--- init.el
+++ init.el
@@ -19,12 +19,12 @@
  ;; If there is more than one, they won't work right.
  )
 
-(add-to-list 'default-frame-alist '(font . "Fira Mono-12"))
+(add-to-list 'default-frame-alist '(font . "Fira Mono-12"))		;; Set the font
 (scroll-bar-mode -1)											;; Disable scrollbar
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(menu-bar-mode -1)
-(setq visible-bell t)
+(tool-bar-mode -1)												;; Disable tool bar
+(tooltip-mode -1)												;; Disable tool tip
+(menu-bar-mode -1)												;; Disable menu bar
+(setq visible-bell t)											;; Enable visual bell
 (setq-default display-line-numbers 'relative)
 (setq-default tab-width 4)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
@@ -56,6 +56,11 @@
   :ensure t
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
 
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
